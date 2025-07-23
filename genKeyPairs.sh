7e35aef85d50fe89f3e3a5dcca6bcedb5be411d1
Zijun Yang <zijunhz@126.com>
2025-07-23 10:37:42 +0800

    key: update gen key script

--- genKeyPairs.sh
+++ genKeyPairs.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+rm -rf private_key.pem
+rm -rf public_key.pem
+
 openssl genpkey -algorithm RSA -out private_key.pem -pkeyopt rsa_keygen_bits:2048
 openssl rsa -pubout -in private_key.pem -out public_key.pem
